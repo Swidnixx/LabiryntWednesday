562ade55370f5d8ceab54d65c265105ba3e2fcaf
Swidnixx <swidnixx@gmail.com>
2023-03-15 20:09:55 +0100

    Kontroler Gracza cz.1

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    float xRotation = 0;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRotation -= mouseY;
+
+        transform.localRotation = Quaternion.Euler( xRotation, 0, 0 );
+    }
+}
