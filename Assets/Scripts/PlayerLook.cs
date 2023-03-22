25b2592bbb6ddf224acc9869988f608c73c2cd83
Swidnixx <swidnixx@gmail.com>
2023-03-22 20:10:09 +0100

    Kontroler Gracza cz. 2

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+    public float sensivity = 250;
+
     float xRotation = 0;
+    Transform player;
     private void Start()
     {
-        
+        player = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensivity;
 
         xRotation -= mouseY;
-
+        xRotation = Mathf.Clamp(xRotation, -70, 60);
         transform.localRotation = Quaternion.Euler( xRotation, 0, 0 );
+
+        player.Rotate(0, mouseX, 0);
     }
 }
